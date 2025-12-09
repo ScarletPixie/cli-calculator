616635721d180cb316ff8a40f4d89097184f6b33
akino <scarletskynnned@gmail.com>
2025-12-08 22:50:36 -0300

    refactor: change the 4 basic arithmetic operation names

--- internal/builtin/builtin.c
+++ internal/builtin/builtin.c
@@ -17,7 +17,7 @@ double _div(double lhs, double rhs)
     return lhs / rhs;
 }
 
-const operation_t sum = {"sum", BINARY, .binary=_sum};
-const operation_t sub = {"sub", BINARY, .binary=_sub};
-const operation_t mul = {"mul", BINARY, .binary=_mul};
-const operation_t div = {"div", BINARY, .binary=_div};
+const operation_t sum = {"+", BINARY, .binary=_sum};
+const operation_t sub = {"-", BINARY, .binary=_sub};
+const operation_t mul = {"*", BINARY, .binary=_mul};
+const operation_t div = {"/", BINARY, .binary=_div};
